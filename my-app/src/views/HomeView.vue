944f759e4bec15890321e9fd545f200f74ecb1b2
SijedD <149448448+SijedD@users.noreply.github.com>
2024-02-08 23:10:24 +0700

    Update HomeView.vue

--- my-app/src/views/HomeView.vue
+++ my-app/src/views/HomeView.vue
@@ -16,23 +16,24 @@
       <div class="ag-format-container">
       </div>
       <div class="ag-courses_item" v-for="product in products" :key="product.id">
-        
-          <div class="ag-courses-item_bg"></div>
 
-          <div class="title">
-            Название: {{ product.name }}
-          </div>
+        <div class="ag-courses-item_bg"></div>
 
-          <div class="ag-courses-item_date-box">
+        <div class="title">
+          Название: {{ product.name }}
+        </div>
+
+        <div class="ag-courses-item_date-box">
             <span class="description">
             Описание: {{ product.description }}
           </span>
-            <p class="price">
+          <p class="price">
             Цена: {{ product.price }}руб.
           </p>
-            <button class="btn" >В корзину</button>
-          </div>
-        
+          <button  v-if="isAuthenticated" @click="addToCart(product)" type="submit" class="btn">В корзину</button>
+          <button  v-if="!isAuthenticated" type="submit" class="btn"><router-link  to="/login" class="btn_cart_link">В корзину</router-link></button>
+        </div>
+
       </div>
     </div>
   </div>
@@ -41,32 +42,59 @@
 
 <script>
 import {auth} from '../components/UserLogin.vue'
-  export default {
-    name: 'HomeView',
-    data() {
-      return {
-        auth: false,
-        products: [],
-        username: '',
-      };
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      products: [],
+      productsInCart: []
+    };
+  },
+  methods: {
+    // Добавление товара в корзину (для демонстрации)
+    async addToCart(product) {
+      const productId = product.id;
+      const url = `https://jurapro.bhuser.ru/api-shop/cart/${productId}`;
+      const userToken = localStorage.getItem('userToken');
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${userToken}`
+          }
+        });
+        if (response.ok) {
+          const existingItemIndex = this.productsInCart.findIndex(item => item.id === product.id);
+          if (existingItemIndex !== -1 && this.productExists(this.productsInCart[existingItemIndex], product)) {
+            this.productsInCart[existingItemIndex].quantity++;
+          } else {
+            this.productsInCart.push({...product, quantity: 1});
+          }
+          const data = await response.json();
+          console.log(data.data.message);
+        } else {
+          console.error("Ошибка добавления товара в корзину:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Ошибка добавления товара в корзину:", error);
+      }
     },
-    methods: {
-      // Добавление товара в корзину (для демонстрации)
-      addToCart(product) {
-        this.cart.push(product)
-        console.log('Добавлен товар в корзину:', product);
-      },
-      // Выход из аккаунта
-      logout() {
+    productExists(item1, item2) {
+      // Функция для проверки идентичности товаров
+      return item1.id === item2.id && item1.name === item2.name && item1.description === item2.description && item1.price === item2.price;
+    },
+    // Выход из аккаунта
+    logout() {
       localStorage.removeItem('userToken');
       this.$router.push('/');
     },
 
-      async getProduct(){
+    async getProduct(){
       const url = "https://jurapro.bhuser.ru/api-shop/products";
       const response = await fetch(url,{
         method: 'GET',
-            headers: {
+        headers: {
           'Content-Type': 'application/json',
         },
 
@@ -82,29 +110,29 @@ import {auth} from '../components/UserLogin.vue'
 
 
     }
-    },
-    computed: {
+  },
+  computed: {
     isAuthenticated() {
       return !!localStorage.getItem('userToken');
     }
   }
-  };
+};
 </script>
 
 <style>
-  .ag-courses_item{
-    border: 1px solid fuchsia;
+.ag-courses_item{
+  border: 1px solid fuchsia;
   padding: 10px;
   margin-bottom: 15px;
   margin-left: 50px;
-  }
-  body{
-    background-color: #42b983;
-  }
-  
-  
+}
+body{
+  background-color: #42b983;
+}
+
+
 h1{
-text-align: center;
+  text-align: center;
 }
 .home-container {
   max-width: 800px;
